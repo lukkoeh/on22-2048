b9ebe6675497120b2ab34a70094ea7e2c4113206
Lukas Köhler <github@lukas-koehler.de>
2022-11-29 19:23:05 +0100

    INITIAL COMMIT - Add Package dhbw.on22 - Add Main Class "Game" - Create empty window

---
+++ dhbw/on22/Game.java
@@ -0,0 +1,16 @@
+package dhbw.on22;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Game extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        //TO-DO Configure App Window here
+    }
+
+    public static void main(String[] args) {
+        launch(args); //Launch the app
+    }
+}
