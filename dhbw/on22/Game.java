8c8dd4ee65db441f3b2b7362538b61e9fe959621
Lukas Köhler <github@lukas-koehler.de>
2022-12-13 11:37:03 +0100

    Create first JFX-GUI Template Creation of a first GUI-sketch using FXML and Labels, no game logic included.

--- dhbw/on22/Game.java
+++ dhbw/on22/Game.java
@@ -1,16 +1,35 @@
 package dhbw.on22;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.*;
 
 public class Game extends Application {
 
+    private static int padding = 5;
     @Override
-    public void start(Stage primaryStage) {
-        //TO-DO Configure App Window here
+    public void start(Stage primaryStage) throws IOException {
+        primaryStage.setTitle("2048 Game");
+        Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
+        Scene mainscene = new Scene(root, 500, 550);
+        mainscene.getStylesheets().add(Game.class.getResource("Game.css").toExternalForm());
+        primaryStage.setScene(mainscene);
+        primaryStage.show();
     }
 
     public static void main(String[] args) {
         launch(args); //Launch the app
     }
+
+
 }
