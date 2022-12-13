ec2e013369f12477424cff362e2707c58a1eec7b
Lukas Köhler <github@lukas-koehler.de>
2022-12-13 12:37:51 +0100

    Create GUI-Interaction methods Create methods to interact with the tiles on the GUI. Provide basic functionality, such as changing values.

--- dhbw/on22/Game.java
+++ dhbw/on22/Game.java
@@ -1,34 +1,61 @@
 package dhbw.on22;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.security.Key;
 import java.util.*;
 
 public class Game extends Application {
 
-    private static int padding = 5;
+    private Parent root;
     @Override
     public void start(Stage primaryStage) throws IOException {
         primaryStage.setTitle("2048 Game");
-        Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
+        root = FXMLLoader.load(getClass().getResource("Main.fxml"));
         Scene mainscene = new Scene(root, 500, 550);
         mainscene.getStylesheets().add(Game.class.getResource("Game.css").toExternalForm());
+        mainscene.setOnKeyPressed(e -> {
+            if (e.getCode() == KeyCode.W) {
+                System.out.println("up");
+            }
+            else if (e.getCode() == KeyCode.A) {
+                System.out.println("left");
+            }
+            else if (e.getCode() == KeyCode.S) {
+                System.out.println("down");
+            }
+            else if (e.getCode() == KeyCode.D) {
+                System.out.println("right");
+            }
+        });
         primaryStage.setScene(mainscene);
         primaryStage.show();
     }
 
     public static void main(String[] args) {
         launch(args); //Launch the app
+    }
+
+    public void updateScore(int score) {
+        Label lbl = (Label) root.lookup("#score");
+        lbl.setText("SCORE: " + score);
+    }
+
+    public void updateGuiTile(int x, int y, int value) {
+        Label lbl = (Label) root.lookup("#x" + x + "y" + y);
+        lbl.setText(Integer.toString(value));
     }
 
 
