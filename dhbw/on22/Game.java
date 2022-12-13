5f5fc50f962f2fc03b1e1ac7e22208a2c2f1db5c
tdoerr <tim.doerr.22@lehre.mosbach.dhbw.de>
2022-12-13 14:03:03 +0100

    Implement board initialization, first 2 random tiles
    On startup, the board gets filled with "0" across all tiles and 2 random tiles are selected to be filled with "2"

--- dhbw/on22/Game.java
+++ dhbw/on22/Game.java
@@ -20,12 +20,22 @@ import java.util.*;
 public class Game extends Application {
 
     private Parent root;
+    Tile[][] board = new Tile[4][4];
     @Override
     public void start(Stage primaryStage) throws IOException {
         primaryStage.setTitle("2048 Game");
         root = FXMLLoader.load(getClass().getResource("Main.fxml"));
         Scene mainscene = new Scene(root, 500, 550);
         mainscene.getStylesheets().add(Game.class.getResource("Game.css").toExternalForm());
+        for(int i = 1; i<=4; i++){
+            for (int j = 1; j<=4; j++){
+                board[i-1][j-1] = new Tile();
+
+            }
+        }
+        randomTile(true);
+        randomTile(true);
+        updateGuiTiles();
         mainscene.setOnKeyPressed(e -> {
             if (e.getCode() == KeyCode.W) {
                 System.out.println("up");
@@ -52,9 +62,29 @@ public class Game extends Application {
         Label lbl = (Label) root.lookup("#score");
         lbl.setText("SCORE: " + score);
     }
+    public void randomTile(boolean two){
+        Random r = new Random();
+        int x = r.nextInt(3);
+        int y = r.nextInt(3);
+        if (board[x][y].getVal() == 0 ){
+            if(two) {
+                board[x][y].setVal(2);
+            }else{
+                board[x][y].setVal((r.nextInt(1)+1)*2);
+            }
+        }else{
+            randomTile(two);
+        }
+    }
+    public void updateGuiTiles() {
+        for(int x = 1; x<=4; x++){
+            for (int y = 1; y<=4; y++){
+                Label lbl = (Label) root.lookup("#x" + x + "y" + y);
+                lbl.setText(Integer.toString(board[x-1][y-1].getVal()));
+                lbl.setBackground(new Background(new BackgroundFill(board[x-1][y-1].getColor(), null, null)));
 
-    public void updateGuiTile(int x, int y, int value) {
-        Label lbl = (Label) root.lookup("#x" + x + "y" + y);
-        lbl.setText(Integer.toString(value));
+            }
+        }
+
     }
 }
